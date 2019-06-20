b497e2409d53b243980362a7c2a3110231c45b33
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-06-20 10:20:10 +0100

    initial working put-together of stuff

---
+++ snow.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+
+import argparse
+#import json
+import pysnow
+import snow_cli
+import yaml
+
+from pprint import pprint
+from types import SimpleNamespace
+
+
+apis = SimpleNamespace(
+    app         = '/table/cmdb_ci_appl',
+    change      = '/table/change_request',
+    incident    = '/table/incident'
+)
+
+
+
+def parse_args():
+    """The CLI arguments."""
+    p = argparse.ArgumentParser()
+    s = p.add_subparsers(dest='resource', required=True)
+
+    # Global
+    p.add_argument('-c', '--config', required=False, default='./snow.yml',
+        help='The config file location')
+
+    app     = s.add_parser('app', help='Appication resource')
+    app_s   = app.add_subparsers(dest='action', required=True)
+
+    ch      = s.add_parser('change', help='Change resource')
+    ch_s    = ch.add_subparsers(dest='action', required=True)
+
+    inc     = s.add_parser('incident', help='Incident resource')
+    inc_s   = inc.add_subparsers(dest='action', required=True)
+
+
+    # App Subparsers
+    app_get = app_s.add_parser('get', help='Get an application configuration item')
+    app_get_id = app_get.add_mutually_exclusive_group(required=True)
+    app_get_id.add_argument('-n', '--name', help='Get the resource by name search')
+    app_get_id.add_argument('-i', '--id', help='Get the resource by id')
+
+    # Change Subparsers
+    ch_approved = ch_s.add_parser('is-approved?',
+        help='Check if a change is in approved state')
+    ch_approved.add_argument('-n', '--number', required=True,
+        help='The change record number (CHGXXXXXXX)')
+
+    # Incident Subparsers
+
+    return p.parse_args()
+
+
+def load_config(_file):
+    """Load a YAML config file and return a dict."""
+    with open(_file) as f:
+        return yaml.safe_load(f)
+
+
+def new_conn(config):
+    """Create a ServiceNow connection and return the object."""
+    return pysnow.Client(instance=config['host'], user=config['user'], password=config['pass'])
+
+
+def resource_handler(sn_conn, api):
+    """Return an API handler usin the API map."""
+    return sn_conn.resource(api_path=apis.__dict__[api])
+
+
+def main():
+    args    = parse_args()
+    config  = load_config(args.config)
+    sn      = new_conn(config)
+    res_h   = resource_handler(sn, args.resource)
+    #for r in snow_cli.handler(res_h, args).all():
+    #    print(json.dumps(r))
+    exit(True != snow_cli.handler(res_h, args))
+
+
+if __name__ == '__main__':
+    main()
---
+++ snow_cli.py
@@ -0,0 +1,49 @@
+import json
+
+def handler(res_h, args):
+    cli_h = {
+        'app': {
+            'get': app_get
+        },
+
+        'change': {
+            'is-approved?': ch_is_approved
+        },
+
+        'incident': {
+            'get': inc_get
+        }
+    }
+
+    return cli_h[args.resource][args.action](res_h, args)
+
+
+def app_get(res_h, args):
+    if args.name is not None:
+        resp = res_h.get(query='name={}'.format(args.name)).all()
+    elif args.id is not None:
+        resp = res_h.get(query='sys_id={}'.format(args.id)).all()
+    else:
+        return False
+
+    # If records, print, else print empty
+    if len(resp) > 0:
+        for r in resp:
+            print(json.dumps(resp))
+        return True
+    else:
+        print('{}')
+        return False
+
+
+def ch_is_approved(res_h, args):
+    record = res_h.get(query='number={}'.format(args.number))
+    if record['approval'] == 'approved':
+        print('STATE=APPROVED')
+        return True
+    else:
+        print('STATE={}'.format(record['approval'].upper()))
+        return False
+
+def inc_get(res_h, args):
+    pass
